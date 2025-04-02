238e39cc77351b2921b84f5429f8ce67e4ad4b67
RicardoSMtz <santmrich@gmail.com>
2025-04-02 01:40:00 -0600

    +Segundo Idioma +Corrección de interfaz +Cumpliendo con los todos los puntos.

--- app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.ejercicio1
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,7 +15,6 @@ import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,7 +25,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         val formulas = arrayOf(
             getString(R.string.string_chicharronera),
             getString(R.string.area_triangulo),
@@ -70,7 +70,7 @@ class MainActivity : AppCompatActivity() {
                         val base = binding.Var1.text.toString().toInt()
                         val altura = binding.Var2.text.toString().toInt()
                         val resultado = areaTriangulo(base, altura)
-                        binding.tvResult.text = "Área: $resultado"
+                        binding.tvResult.text = getString(R.string.displayarea, resultado)
                     }
                 }
                 getString(R.string.velocidad) -> {
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity() {
                         val distancia = binding.Var1.text.toString().toInt()
                         val tiempo = binding.Var2.text.toString().toInt()
                         val resultado = calcularVelocidad(distancia, tiempo)
-                        binding.tvResult.text = "Velocidad: $resultado"
+                        binding.tvResult.text = getString(R.string.displayVelocidad, resultado)
                     }
                 }
                 getString(R.string.perimetro_rectangulo) -> {
@@ -86,7 +86,7 @@ class MainActivity : AppCompatActivity() {
                         val lado1 = binding.Var1.text.toString().toInt()
                         val lado2 = binding.Var2.text.toString().toInt()
                         val resultado = perimetroRectangulo(lado1, lado2)
-                        binding.tvResult.text = "Perímetro: $resultado"
+                        binding.tvResult.text = getString(R.string.displayPerimetro, resultado)
                     }
                 }
             }
@@ -98,97 +98,99 @@ class MainActivity : AppCompatActivity() {
 
         when (position) {
             0 -> { // Fórmula general
-                binding.Var1.hint = "Coeficiente a"
-                binding.Var2.hint = "Coeficiente b"
-                binding.Var3.hint = "Coeficiente c"
-                binding.a.text = "a:"
-                binding.b.text = "b:"
+                binding.Var1.hint = getString(R.string.coeficientea)
+                binding.Var2.hint = getString(R.string.coeficienteb)
+                binding.Var3.hint = getString(R.string.coeficientec)
+                binding.a.text = getString(R.string.adospuntos)
+                binding.b.text = getString(R.string.bdospuntos)
                 binding.Var3.visibility = View.VISIBLE
                 binding.c.visibility = View.VISIBLE
                 binding.Var3.isEnabled = true // Deshabilitar Var3 ya que no es necesario
-                // Deshabilitar campos no necesarios
             }
             1 -> { // Área del triángulo
-                binding.Var1.hint = "Base"
-                binding.Var2.hint = "Altura"
-                binding.a.text = "b:"
-                binding.b.text = "h:"
+                binding.Var1.hint = getString(R.string.basee)
+                binding.Var2.hint = getString(R.string.alturaa)
+                binding.a.text = getString(R.string.bdospuntos)
+                binding.b.text = getString(R.string.hdospuntos)
                 binding.Var3.visibility = View.INVISIBLE
                 binding.c.visibility = View.INVISIBLE
                 binding.Var3.isEnabled = false // Deshabilitar Var3 ya que no es necesario
             }
             2 -> { // Velocidad
-                binding.Var1.hint = "Distancia"
-                binding.Var2.hint = "Tiempo"
-                binding.a.text = "d:"
-                binding.b.text = "t:"
+                binding.Var1.hint = getString(R.string.distanciaa)
+                binding.Var2.hint = getString(R.string.tiempoo) //Tiempo
+                binding.a.text = getString(R.string.ddospuntos) //d
+                binding.b.text = getString(R.string.tdospuntos)//t
                 binding.Var3.visibility = View.INVISIBLE
                 binding.c.visibility = View.INVISIBLE
                 binding.Var3.isEnabled = false // Deshabilitar Var3 ya que no es necesario
             }
             3 -> { // Perímetro del rectángulo
-                binding.Var1.hint = "Lado 1"
-                binding.Var2.hint = "Lado 2"
-                binding.a.text = "l1:"
-                binding.b.text = "l2:"
+                binding.Var1.hint = getString(R.string.lado1) //L1
+                binding.Var2.hint = getString(R.string.lado2) //L2
+                binding.a.text = getString(R.string.ele1)//"l1:"
+                binding.b.text = getString(R.string.ele2)//"l2:"
                 binding.Var3.visibility = View.INVISIBLE
                 binding.c.visibility = View.INVISIBLE
                 binding.Var3.isEnabled = false // Deshabilitar Var3 ya que no es necesario
-            }
-        }
-    }
-
+             }
+         }
+     }
     private fun validarCamposVacios(numCampos: Int): Boolean {
         var valido = true
 
         // Validar Var1
         if (binding.Var1.visibility == View.VISIBLE && binding.Var1.text.toString().isEmpty()) {
-            binding.Var1.error = "Se requiere un número "
+            binding.Var1.error = getString(R.string.requierenum)
             binding.Var1.requestFocus()
             valido = false
         } else if (binding.Var1.visibility == View.VISIBLE && binding.Var1.text.toString().toIntOrNull() == 0) {
-            binding.Var1.error = "Debe ser diferente de 0"
+            binding.Var1.error = getString(R.string.difcero)
             binding.Var1.requestFocus()
             valido = false
-        }
+         }
 
         // Validar Var2
         if (binding.Var2.visibility == View.VISIBLE && binding.Var2.text.toString().isEmpty()) {
-            binding.Var2.error = "Se requiere un número"
+            binding.Var2.error = getString(R.string.requierenum)
             binding.Var2.requestFocus()
             valido = false
         } else if (binding.Var2.visibility == View.VISIBLE && binding.Var2.text.toString().toIntOrNull() == 0) {
-            binding.Var2.error = "Debe ser diferente de 0"
+            binding.Var2.error = getString(R.string.difcero)
             binding.Var2.requestFocus()
             valido = false
-        }
+         }
 
         // Validar Var3
         if (numCampos == 3 && binding.Var3.visibility == View.VISIBLE && binding.Var3.text.toString().isEmpty()) {
-            binding.Var3.error = "Se requiere un número"
+            binding.Var3.error = getString(R.string.requierenum)
             binding.Var3.requestFocus()
             valido = false
         } else if (numCampos == 3 && binding.Var3.visibility == View.VISIBLE && binding.Var3.text.toString().toIntOrNull() == 0) {
-            binding.Var3.error = "Debe ser diferente de 0"
+            binding.Var3.error = getString(R.string.difcero)
             binding.Var3.requestFocus()
             valido = false
-        }
-
+         }
         return valido
+
     }
 
 
+    //Función para calcular fórmula general
     private fun formGeneral(a: Int, b: Int, c: Int): String {
         val discriminante = b * b - 4 * a * c
-        return if (discriminante < 0) "No hay soluciones reales"
+        return if (discriminante < 0) getString(R.string.solucionesreales)
         else {
             val raiz1 = (-b + sqrt(discriminante.toDouble())) / (2 * a)
             val raiz2 = (-b - sqrt(discriminante.toDouble())) / (2 * a)
-            "Raíz 1: ${"%.2f".format(raiz1)}, Raíz 2: ${"%.2f".format(raiz2)}"
+            getString(R.string.displayChicarronera, raiz1, raiz2)
         }
     }
-
+    //Función para calcular el área del triángulo
     private fun areaTriangulo(base: Int, altura: Int) = (base * altura) / 2.0
+    //Función para calcular velocidad
     private fun calcularVelocidad(distancia: Int, tiempo: Int) = distancia / tiempo.toDouble()
+    //Función para perímetro
     private fun perimetroRectangulo(lado1: Int, lado2: Int) = 2 * (lado1 + lado2)
+
 }
